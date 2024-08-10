2caf3008d9e91bedd7505d2cdb6c623a1701d3da
xiracuko <matvey.a.a.7@gmail.com>
2024-08-10 08:36:23 +0300

    build: initialization of ResultBlock.tsx component + redux logic

---
+++ src/components/ResultBlock.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useAppDispatch, useAppSelector } from "../hooks/useAppRedux";
+import { restartTyping } from "../redux/services/typingSlice";
+
+const ResultBlock: React.FC = () => {
+  const { errors, wpm } = useAppSelector((state) => state.typingSlice);
+  const dispatch = useAppDispatch();
+
+  const handleRestart = () => dispatch(restartTyping());
+
+  return (
+    <div>
+      <p>Errors: {errors}</p>
+      <p>Wpm: {wpm}</p>
+      <button onClick={handleRestart}>restart</button>
+    </div>
+  )
+}
+
+export default ResultBlock;
