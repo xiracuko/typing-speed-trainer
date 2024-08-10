f2f98f8688b1908def91c9e4cf3f935a80777a80
xiracuko <matvey.a.a.7@gmail.com>
2024-08-10 08:43:48 +0300

    feature: adding logic to display a specific component based on a condition

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import TypingBlock from './components/TypingBlock';
+import ResultBlock from './components/ResultBlock';
+import { useAppSelector } from './hooks/useAppRedux';
 
 const App: React.FC = () => {
+  const completed = useAppSelector((state) => state.typingSlice.completed);
 
   return (
-    <div className="App">App</div>
+    <div className="App">
+      {completed ? <ResultBlock /> : <TypingBlock />}
+    </div>
   );
 }
 
