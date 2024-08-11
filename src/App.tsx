ef1f85cb65b23a494c83778e01b1875ca12ce035
xiracuko <matvey.a.a.7@gmail.com>
2024-08-11 17:53:59 +0300

    build: initializing styles in a project (library/files)

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import TypingBlock from './components/TypingBlock';
 import ResultBlock from './components/ResultBlock';
 import { useAppSelector } from './hooks/useAppRedux';
+import "./scss/style.scss"
 
 const App: React.FC = () => {
   const completed = useAppSelector((state) => state.typingSlice.completed);
 
   return (
-    <div className="App">
+    <div className="container">
       {completed ? <ResultBlock /> : <TypingBlock />}
     </div>
   );
